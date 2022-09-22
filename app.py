8b8651d22f832a09c5072fcde4bddcb9ee50c7ba
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2022-09-22 12:31:32 -0700

    add orangecluster

--- app.py
+++ app.py
@@ -155,7 +155,8 @@ frontdesk_frontend_stack = LSITStack(
             "antsocmsa.advisingfrontdesk.lsit.ucdavis.edu",
             "caes.advisingfrontdesk.lsit.ucdavis.edu",
             "cs.advisingfrontdesk.lsit.ucdavis.edu",
-            "mae.advisingfrontdesk.lsit.ucdavis.edu"
+            "mae.advisingfrontdesk.lsit.ucdavis.edu",
+            "orangecluster.advisingfrontdesk.lsit.ucdavis.edu"
         ],
         "certificate_arns": ["arn:aws:acm:us-west-2:042277129213:certificate/a9930618-8c81-482e-b43c-0e9d1f06b616", "arn:aws:acm:us-west-2:042277129213:certificate/22c71dbb-f075-456a-b9e7-c6c08df51837"],
         "is_private": True,
