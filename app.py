4190081ada97d502da3ee76d44360a0159ae5113
Edgar Duran-Perez <eduranperez@ucdavis.edu>
2023-07-10 10:28:09 -0700

    add subdomains

--- app.py
+++ app.py
@@ -172,7 +172,8 @@ frontdesk_frontend_stack = LSITStack(
             "engineering.advisingfrontdesk.lsit.ucdavis.edu",
             "acbnc.advisingfrontdesk.lsit.ucdavis.edu",
             "oeoes.advisingfrontdesk.lsit.ucdavis.edu",
-            "lgbtqia.advisingfrontdesk.lsit.ucdavis.edu"
+            "lgbtqia.advisingfrontdesk.lsit.ucdavis.edu",
+            "menasa.advisingfrontdesk.lsit.ucdavis.edu"
         ],
         "certificate_arns": ["arn:aws:acm:us-west-2:042277129213:certificate/692cfbeb-e216-4319-890d-47d2283b1429", "arn:aws:acm:us-west-2:042277129213:certificate/22c71dbb-f075-456a-b9e7-c6c08df51837"],
         "is_private": True,
