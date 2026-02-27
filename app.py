3e7ddf2ddfb17556dd747e64a5da77fbfb1ed3d6
Edgar Perez <eduranperez@ucdavis.edu>
2026-02-27 15:19:03 -0800

    add esp domain

--- app.py
+++ app.py
@@ -196,6 +196,7 @@ frontdesk_frontend_stack = LSITStack(
             "bao.advisingfrontdesk.lsit.ucdavis.edu",
             "ggcs.advisingfrontdesk.lsit.ucdavis.edu",
             "asac.advisingfrontdesk.lsit.ucdavis.edu",
+            "esp.advisingfrontdesk.lsit.ucdavis.edu",
         ],
         "certificate_arns": ["arn:aws:acm:us-west-2:042277129213:certificate/1a7b4790-62e1-460a-aa90-52d63fda54d9", "arn:aws:acm:us-west-2:042277129213:certificate/6da950e7-0463-406d-b80f-077d5f20226b"],
         "is_private": True,
